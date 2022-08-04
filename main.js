f34de79a8684a592ed4768734ee6388c27336c74
da-dfdf <88136653+da-dfdf@users.noreply.github.com>
2022-08-04 10:07:00 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,22 @@ status = "";
 objects = [];
 
 function preload(){
-     img = loadImage("dog_cat.jpg");
+    
 }
 
 function setup(){
-    canvas = createCanvas(640,420);
+    canvas = createCanvas(380,380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
 
+    
+}
+
+function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detection Started";
 }
 
 function modelLoaded(){
@@ -18,7 +26,7 @@ function modelLoaded(){
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
     status = true;
 
-    objectDetector.detect(img, gotResults);
+    
      
 }
     
@@ -33,20 +41,26 @@ function gotResults(error, results){
 }
 
 function draw(){
-    image(img,0,0,640,420);
+    image(video,0,0,380,380);
 
     if(status != ""){
-        for(i = 0; i<objects.length; i++){
-            document.getElementById("status").innerHTML = "Status: Objects Detected";
+        
 
-            fill("red");
-            stroke("red");
+        objectDetector.detect(video, gotResults);
+        for(i = 0; i<objects.length; i++){
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("objectNumber").innerHTML = "Number of objects are:" + objects.length;
+            fill(r,g,b);
+            stroke(r,g,b);
             
             accuracy = floor(objects[i].confidence*100);
 
             text(objects[i].label + " " + accuracy + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
-            rect(objects[i].x, objects[i].y, objects[i].width - 50, objects[i].height);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 
         }
     }
