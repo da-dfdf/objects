7bdc3c0e87c229997555ec67e44b982c0c2c72f7
da-dfdf <88136653+da-dfdf@users.noreply.github.com>
2022-08-09 10:06:26 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,37 +2,44 @@ img = "";
 status = "";
 objects = [];
 
-function preload(){
-    
+function preload() {
+
 }
 
-function setup(){
-    canvas = createCanvas(380,380);
+function setup() {
+    canvas = createCanvas(380, 380);
     canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380,380);
+    video = createCapture({
+        audio: false,
+        video: {
+            facingMode: {
+                exact: "environment"
+            }
+        }
+    });
+    video.size(380, 380);
     video.hide();
 
-    
+
 }
 
-function start(){
+function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detection Started";
 }
 
-function modelLoaded(){
+function modelLoaded() {
     console.log("model loaded");
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
     status = true;
 
-    
-     
-}
-    
 
-function gotResults(error, results){
-    if(error){
+
+}
+
+
+function gotResults(error, results) {
+    if (error) {
         console.log(error);
     }
 
@@ -40,23 +47,23 @@ function gotResults(error, results){
     objects = results;
 }
 
-function draw(){
-    image(video,0,0,380,380);
+function draw() {
+    image(video, 0, 0, 380, 380);
 
-    if(status != ""){
-        
+    if (status != "") {
+
 
         objectDetector.detect(video, gotResults);
-        for(i = 0; i<objects.length; i++){
+        for (i = 0; i < objects.length; i++) {
             r = random(255);
             g = random(255);
             b = random(255);
             document.getElementById("status").innerHTML = "Status: Objects Detected";
             document.getElementById("objectNumber").innerHTML = "Number of objects are:" + objects.length;
-            fill(r,g,b);
-            stroke(r,g,b);
-            
-            accuracy = floor(objects[i].confidence*100);
+            fill(r, g, b);
+            stroke(r, g, b);
+
+            accuracy = floor(objects[i].confidence * 100);
 
             text(objects[i].label + " " + accuracy + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
@@ -65,5 +72,5 @@ function draw(){
         }
     }
 
-    
+
 }
